e272d005b2ec5d135c2580c825c59da975b4911e
J3anP <a20213733@pucp.edu.pe>
2024-06-27 19:17:06 -0500

    first commit

---
+++ src/test/java/org/example/lab10gtics/Lab10GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab10gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab10GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
